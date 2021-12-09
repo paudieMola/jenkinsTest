cf188f30e71fb16559b6cd3429ccd4e9774eeb39
Helen Daly <helen.daly@mycit.ie>
2021-12-09 10:16:52 +0000

    clean up

--- helloWorld.java
+++ helloWorld.java
@@ -2,9 +2,6 @@ public class helloWorld
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello My World");
-		System.out.println("Hello My World Again");
-		System.out.println("Test push to Github");
-		System.out.println("Demo push to Github!!");
+		System.out.println("Hello World");
 	}
 }
