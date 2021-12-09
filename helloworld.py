d318f22a12d7e9dfb59073a4811ef34204dce241
Helen Daly <helen.daly@mycit.ie>
2021-12-09 20:22:46 +0000

    de

--- helloworld.py
+++ helloworld.py
@@ -2,9 +2,8 @@ def helloworld():
 	f = open('helloworld.txt', 'w')
 	f.write('Hello World')
 	f.close()
-	print('Hello Python World!')
-	print('python file created')
+	print('Hello Python World')
+	print('Python file created.')
 	
-
 if __name__ == '__main__':
     helloworld()
